ba075cab1c1af3484712e93f8813ea203a420555
Patryk Gajek <pgajek94@gmail.com>
2020-03-16 23:16:13 +0100

    navigation ready to go, first declarations of header

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,5 +1,7 @@
 const burger = document.querySelector(".navigation__burger");
+const nav = document.querySelector(".navigation");
 export function handleBurgerClick() {
   burger.classList.toggle("navigation__burger--active");
+  nav.classList.toggle("navigation--active");
 }
 burger.addEventListener("click", handleBurgerClick);
