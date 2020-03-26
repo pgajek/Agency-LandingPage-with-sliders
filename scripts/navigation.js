75054e4a16b98d1a8f7c9abeee1d421d437f5f24
Patryk Gajek <pgajek94@gmail.com>
2020-03-26 14:52:18 +0100

    nav received some onScroll event

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -5,3 +5,17 @@ export function handleBurgerClick() {
   nav.classList.toggle("navigation--active");
 }
 burger.addEventListener("click", handleBurgerClick);
+
+function CheckNavPosition() {
+  const section = document.querySelector(".aboutUs");
+  let Break = section.getBoundingClientRect().top;
+  console.log(Break, window.innerHeight);
+  if (Break <= 0) {
+    console.log("yes");
+    nav.classList.add("navigation--bgc");
+  } else {
+    nav.classList.remove("navigation--bgc");
+  }
+}
+
+window.addEventListener("scroll", CheckNavPosition);
