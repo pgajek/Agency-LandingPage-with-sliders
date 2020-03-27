6f7ebd9a8a76e6f764dba9b2c00b3564709082ff
Patryk Gajek <pgajek94@gmail.com>
2020-03-27 21:24:37 +0100

    scripts written for navigation

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+import Slider from "./slider.js";
+import { CheckNavPosition, HandleBurgerClick } from "./navigation.js";
+
+const burger = document.querySelector(".navigation__burger");
+const nav = document.querySelector(".navigation");
+
+window.addEventListener("scroll", () => CheckNavPosition(nav));
+burger.addEventListener("click", () => HandleBurgerClick(nav, burger));
--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,21 +1,14 @@
-const burger = document.querySelector(".navigation__burger");
-const nav = document.querySelector(".navigation");
-export function handleBurgerClick() {
+export function HandleBurgerClick(nav, burger) {
   burger.classList.toggle("navigation__burger--active");
   nav.classList.toggle("navigation--active");
 }
-burger.addEventListener("click", handleBurgerClick);
 
-function CheckNavPosition() {
+export function CheckNavPosition(nav) {
   const section = document.querySelector(".aboutUs");
   let Break = section.getBoundingClientRect().top;
-  console.log(Break, window.innerHeight);
   if (Break <= 0) {
-    console.log("yes");
     nav.classList.add("navigation--bgc");
   } else {
     nav.classList.remove("navigation--bgc");
   }
 }
-
-window.addEventListener("scroll", CheckNavPosition);
