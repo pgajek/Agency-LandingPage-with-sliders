0435b33bc1ffed926a89a0855a80bf6c75e06a53
Patryk Gajek <pgajek94@gmail.com>
2020-04-20 17:16:35 +0200

    process section went responsive, and has a swiper in addition

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,10 @@ import Slider from "./slider.js";
 import {
   CheckNavPosition,
   HandleBurgerClick,
-  HandleNavLinkClick
+  HandleNavLinkClick,
 } from "./navigation.js";
 import ChangeClient from "./clients.js";
+import { swipingStart, swipingMove, swipingEnd } from "./swiper.js";
 
 const burger = document.querySelector(".navigation__burger");
 const nav = document.querySelector(".navigation");
@@ -12,7 +13,7 @@ const navLinks = document.querySelectorAll(".navigation__link");
 
 window.addEventListener("scroll", () => CheckNavPosition(nav));
 burger.addEventListener("click", () => HandleBurgerClick(nav, burger));
-navLinks.forEach(link =>
+navLinks.forEach((link) =>
   link.addEventListener("click", () => HandleNavLinkClick(nav, burger))
 );
 const TeamSlider = new Slider(".team__slide", true);
@@ -21,7 +22,24 @@ const smallClients = document.querySelectorAll(".clients__client--small");
 const allItems = document.querySelectorAll("[data-client]");
 
 for (const client of smallClients) {
-  client.addEventListener("click", e =>
+  client.addEventListener("click", (e) =>
     ChangeClient(e, smallClients, allItems)
   );
 }
+
+const swiper = document.querySelector(".process__boxWrapper");
+
+swiper.addEventListener("mouseleave", () => {
+  isDown = false;
+});
+
+swiper.addEventListener("mouseup", () => {
+  isDown = false;
+});
+
+swiper.addEventListener("touchstart", swipingStart);
+swiper.addEventListener("mousedown", swipingStart);
+swiper.addEventListener("touchmove", swipingMove);
+swiper.addEventListener("mousemove", swipingMove);
+swiper.addEventListener("touchend", swipingEnd);
+swiper.addEventListener("mouseup", swipingEnd);
---
+++ scripts/swiper.js
@@ -0,0 +1,35 @@
+let isDown = false;
+let startX;
+let scrollLeft;
+let eventX;
+let walk;
+const swiper = document.querySelector(".process__boxWrapper");
+
+export const swipingStart = (e) => {
+  if (e.type === "mousedown") {
+    eventX = e.pageX;
+  } else if (e.type === "touchstart") {
+    eventX = e.touches[0].pageX;
+  }
+
+  isDown = true;
+  startX = eventX - swiper.offsetLeft;
+  scrollLeft = swiper.scrollLeft;
+};
+
+export const swipingMove = (e) => {
+  e.preventDefault();
+  if (!isDown) return;
+  if (e.type === "mousemove") {
+    eventX = e.pageX;
+  } else if (e.type === "touchmove") {
+    eventX = e.touches[0].pageX;
+  }
+  const x = eventX - swiper.offsetLeft;
+  walk = x - startX;
+
+  swiper.scrollLeft = scrollLeft - walk;
+};
+export const swipingEnd = () => {
+  isDown = false;
+};
